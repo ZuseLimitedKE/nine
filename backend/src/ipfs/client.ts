8c78b95e456db93420c1be07b6f288322c275216
romannjoroge <roman.njoroge@njuguna.com>
2024-10-11 22:32:48 +0300

    feat: Adding IPFS functionality

---
+++ backend/src/ipfs/client.ts
@@ -0,0 +1,9 @@
+import "dotenv/config";
+import {PinataSDK} from 'pinata';
+
+const pinata = new PinataSDK({
+    pinataJwt: process.env.PINARA_KEY,
+    pinataGateway: process.env.PINATA_GATEWAY
+});
+
+export default pinata;
